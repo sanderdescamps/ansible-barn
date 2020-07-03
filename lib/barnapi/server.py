0ee4082083c1fe5d18c1ea3dd4aae018b1f9a8db
Sander Descamps <sander.descamps@fednot.be>
2020-07-03 15:56:43 +0200

    re-work API

--- lib/barnapi/server.py
+++ lib/barnapi/server.py
@@ -4,10 +4,11 @@ import datetime
 from functools import wraps
 import jwt
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask import request, jsonify, make_response
+from flask import request, jsonify, make_response, redirect, url_for
 from app import app
 from app.models import User, Host, Group, Node
 from app.debug import db_init, db_flush
+from mongoengine.errors import NotUniqueError
 
 
 def authenticate(*roles):
@@ -46,7 +47,7 @@ def authenticate(*roles):
                 return jsonify({
                     'message': 'Not permited, missing roles (%s)' % (','.join(missing_roles))
                     })
-            return f(current_user, *args, **kwargs)
+            return f(*args, current_user=current_user, **kwargs)
 
         return decorator
     return require_token
@@ -78,7 +79,7 @@ def login_user():
     if check_password_hash(user.password_hash, auth.password):
         token = jwt.encode({'public_id': user.public_id, 'exp': datetime.datetime.utcnow(
         ) + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
-        print(token)
+        print("token: %s"%token.decode('UTF-8'))
         return jsonify({'token': token.decode('UTF-8')})
 
     return make_response('could not verify', 401, {
@@ -91,118 +92,205 @@ def get_all_users():
     users = User.objects()
     return jsonify({"result":users.exclude("id")})
 
-@app.route('/host/add', methods=['PUT'])
-@authenticate('AddHost')
-def host_add(current_user):
-    data = request.get_json()
-    if "name" not in data:
-        return jsonify({'message': ''''"name" required argument'''})
-    Host(name=data.get('name'), vars=data.get(
-        'vars', {}), groups=data.get("groups", [])).save()
+@app.route('/nodes', methods=['GET'])
+@authenticate('getNode')
+def get_nodes(current_user=None):
+    args = request.args
+
+    query_args=dict()
+    if "name" in request.args: 
+        query_args["name"] = request.args.get("name")
+    if "type" in request.args:
+        t = request.args.get("type")
+        if t.lower() == "host":
+            query_args["_cls"] = "Node.Host"
+        elif t.lower() == "group":
+            query_args["_cls"] = "Node.Group"
+    o_nodes = Node.objects(**query_args) 
+    return jsonify({'results': o_nodes})
+
+@app.route('/hosts', methods=['GET'])
+@authenticate()
+def alias_hosts(current_user=None):
+    return redirect(url_for('get_nodes', type="host", **request.args))
+
+@app.route('/groups', methods=['GET'])
+@authenticate('getGroup')
+def get_groups(current_user=None):
+    return redirect(url_for('get_nodes', type="group", **request.args))
+
+@app.route('/nodes', methods=['POST'])
+@authenticate('addNode')
+def post_nodes(current_user=None):
+    query_args=dict()
+    if "name" in request.args: 
+        query_args["name"] = request.args.get("name")
+    else:
+        return make_response('name not defined', 400)
+
+    if "type" not in request.args: 
+        return make_response('type not defined', 400)
+
+    try:
+        t = request.args.get("type")
+        if t.lower() == "host":
+            if "groups" in request.args:
+                groups = request.args.get("groups").split(',')
+                o_groups = Group.objects(name__in=groups)
+                query_args["groups"] = o_groups
+            Host(**query_args).save()
+        elif t.lower() == "group":
+            if "parent_groups" in request.args:
+                parent_groups = request.args.get("parent_groups").split(',')
+                o_parent_groups = Group.objects(name__in=parent_groups)
+                query_args["parent_groups"] = o_parent_groups
+            if "child_groups" in request.args:
+                child_groups = request.args.get("child_groups").split(',')
+                o_child_groups = Group.objects(name__in=child_groups)
+                query_args["child_groups"] = o_child_groups
+            Group(**query_args).save()
+        else:
+            return make_response('unknown type: %s'%(t), 400)
+    except NotUniqueError as e:
+        return make_response('Duplicate Node: %s already exist'%(request.args.get("name")), 400)
+
     return jsonify({'message': 'Host Added'})
 
-@app.route('/host/delete', methods=['DELETE'])
-@authenticate('DeleteHost')
-def host_delete(current_user):
-    data = request.get_json()
-    if "name" not in data:
-        return jsonify({'message': ''''"name" required argument'''})
+@app.route('/nodes', methods=['DELETE'])
+@authenticate('deleteNode')
+def delete_nodes(current_user=None):
+    query_args=dict()
+    if "name" in request.args: 
+        query_args["name__in"] = request.args.get("name").split(',')
+    else:
+        return make_response('name not defined', 400)
     
-    if isinstance(data.get("name"), str):
-        data["name"] = [data.get("name")]
-    o_node = Node.objects(name__in=data.get("name")).delete()
+    o_nodes = Node.objects(**query_args)
+    if o_nodes.count() < 1:
+        return make_response('%s not found'%(request.args.get('name')), 400)
+    s_nodes = ','.join(o_nodes.scalar('name'))
+    o_nodes.delete()
+    return jsonify({'message': '%s have been deleted'%(s_nodes)})
 
-    return jsonify({'message': 'Host deleted'})
 
-@app.route('/groupadd', methods=['PUT'])
-@authenticate('AddGroup')
-def group_add(current_user):
-    warning = []
-    data = request.get_json()
-    if "name" not in data:
-        return jsonify({'message': ''''"name" required argument'''})
+@app.route('/test', methods=['GET'])
+@authenticate()
+def test(current_user=None):
+    return make_response("'name' required parameter", 400) 
 
-    group = Group(name=data.get('name'), vars=data.get('vars', {}))
-    group.save()
-    parentgroups = data.get("parent_groups", None)
-    if parentgroups is not None:
-        o_parentgroup = Group.objects(name__in=parentgroups)
-        if o_parentgroup is not None:
-            group.parent_groups.extend(o_parentgroup)
-            for g in o_parentgroup:
-                g.child_groups.append(group)
-                g.save()
-        for not_found in Group.objects(name__nin=parentgroups):
-            warning.append("Could not find parent group %s" % (not_found))
 
-    childgroups = data.get("child_groups", None)
-    if childgroups is not None:
-        o_childgroups = Group.objects(name__in=childgroups)
-        if o_childgroups is not None:
-            group.child_groups.extend(o_childgroups)
-            for g in o_childgroups:
-                g.parent_groups.append(group)
-                g.save()
-        for not_found in Group.objects(name__nin=childgroups):
-            warning.append("Could not find child group %s" % (not_found))
+# @app.route('/host/add', methods=['PUT'])
+# @authenticate('AddHost')
+# def host_add(current_user=None):
+#     data = request.get_json()
+#     if "name" not in data:
+#         return jsonify({'message': ''''"name" required argument'''})
+#     Host(name=data.get('name'), vars=data.get(
+#         'vars', {}), groups=data.get("groups", [])).save()
+#     return jsonify({'message': 'Host Added'})
 
-    group.save()
-    return jsonify({'message': 'Group Added'})
+# @app.route('/host/delete', methods=['DELETE'])
+# @authenticate('DeleteHost')
+# def host_delete(current_user=None):
+#     data = request.get_json()
+#     if "name" not in data:
+#         return jsonify({'message': ''''"name" required argument'''})
+    
+#     if isinstance(data.get("name"), str):
+#         data["name"] = [data.get("name")]
+#     o_node = Node.objects(name__in=data.get("name")).delete()
+
+#     return jsonify({'message': 'Host deleted'})
+
+# @app.route('/groupadd', methods=['PUT'])
+# @authenticate('AddGroup')
+# def group_add(current_user=None):
+#     warning = []
+#     data = request.get_json()
+#     if "name" not in data:
+#         return jsonify({'message': ''''"name" required argument'''})
+
+#     group = Group(name=data.get('name'), vars=data.get('vars', {}))
+#     group.save()
+#     parentgroups = data.get("parent_groups", None)
+#     if parentgroups is not None:
+#         o_parentgroup = Group.objects(name__in=parentgroups)
+#         if o_parentgroup is not None:
+#             group.parent_groups.extend(o_parentgroup)
+#             for g in o_parentgroup:
+#                 g.child_groups.append(group)
+#                 g.save()
+#         for not_found in Group.objects(name__nin=parentgroups):
+#             warning.append("Could not find parent group %s" % (not_found))
+
+#     childgroups = data.get("child_groups", None)
+#     if childgroups is not None:
+#         o_childgroups = Group.objects(name__in=childgroups)
+#         if o_childgroups is not None:
+#             group.child_groups.extend(o_childgroups)
+#             for g in o_childgroups:
+#                 g.parent_groups.append(group)
+#                 g.save()
+#         for not_found in Group.objects(name__nin=childgroups):
+#             warning.append("Could not find child group %s" % (not_found))
+
+#     group.save()
+#     return jsonify({'message': 'Group Added'})
 
 
 @app.route('/init', methods=['PUT'])
 @authenticate()
-def init(current_user):
+def init(current_user=None):
     db_init()
     return jsonify({'message': 'Database has been reseted'})
 
 
 @app.route('/flush', methods=['DELETE'])
 @authenticate("admin")
-def flush(current_user):
+def flush(current_user=None):
     db_flush()
     return jsonify({'message': 'Database has been cleared'})
 
 
-@app.route('/query', methods=['GET', 'POST'])
-@authenticate("ReadOnly")
-def query(current_user):
-    data = request.get_json()
-    if "name" not in data:
-        return jsonify({'message': ''''"name" required argument'''})
+# @app.route('/query', methods=['GET', 'POST'])
+# @authenticate("ReadOnly")
+# def query(current_user=None):
+#     data = request.get_json()
+#     if "name" not in data:
+#         return jsonify({'message': ''''"name" required argument'''})
 
-    nodes = data.get("name")   
-    if isinstance(data.get("name"), str):
-        nodes = [nodes]
-    o_nodes = Node.objects(name__in=nodes)
+#     nodes = data.get("name")   
+#     if isinstance(data.get("name"), str):
+#         nodes = [nodes]
+#     o_nodes = Node.objects(name__in=nodes)
 
-    result = {}
-    for n in nodes:
-        o_node = Node.objects(name=n).first()
-        if o_node: 
-            d_node = o_node.to_dict()
-            d_node["failed"] = False
-            result[n]=d_node
-        else:
-            result[n]={
-                "msg": "Node not found",
-                "failed": True
-            }
-    return jsonify(result)
+#     result = {}
+#     for n in nodes:
+#         o_node = Node.objects(name=n).first()
+#         if o_node: 
+#             d_node = o_node.to_dict()
+#             d_node["failed"] = False
+#             result[n]=d_node
+#         else:
+#             result[n]={
+#                 "msg": "Node not found",
+#                 "failed": True
+#             }
+#     return jsonify(result)
 
-@app.route('/setvar', methods=['PUT'])
-@authenticate("ReadOnly")
-def set_var(current_user):
-    data = request.get_json()
-    if "name" not in data:
-        return jsonify({'message': ''''"name" required argument'''})
-    o_node = Node.objects(name=data.get("name")).first()
-    if o_node is not None:
-        o_node.vars.update(data.get("vars",{}))
-        o_node.save()
-    else:
-        return jsonify({'message': 'host not found'})
-    return jsonify({'message': 'add variables to object', "result": o_node })
+# @app.route('/setvar', methods=['PUT'])
+# @authenticate("ReadOnly")
+# def set_var(current_user=None):
+#     data = request.get_json()
+#     if "name" not in data:
+#         return jsonify({'message': ''''"name" required argument'''})
+#     o_node = Node.objects(name=data.get("name")).first()
+#     if o_node is not None:
+#         o_node.vars.update(data.get("vars",{}))
+#         o_node.save()
+#     else:
+#         return jsonify({'message': 'host not found'})
+#     return jsonify({'message': 'add variables to object', "result": o_node })
 
 
 
