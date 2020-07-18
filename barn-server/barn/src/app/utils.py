53ac36976dc9803ef584c133a366a53991cfb912
Sander Descamps <sander_descamps@hotmail.com>
2020-07-18 22:53:58 +0200

    add remove_underscore_keys

--- barn-server/barn/src/app/utils.py
+++ barn-server/barn/src/app/utils.py
@@ -25,3 +25,21 @@ def merge_args_data(args, data=None):
         for k, v in data.items():
             output[k] = v
     return output
+
+def remove_underscore_keys(d):
+    """
+      Recursivly remove underscore keys from directory or list of directories
+    """
+    res = {}
+    if type(d) == dict:
+        for k, v in d.items():
+            if k.startswith('_'):
+                pass
+            elif type(v) == dict or type(v) == list:
+                res[k] = remove_underscore_keys(v)
+            else:
+                res[k] = v
+    elif type(d) == list:
+        res = list(map(lambda x: remove_underscore_keys(x)
+                       if type(x) == dict else x, d))
+    return res
