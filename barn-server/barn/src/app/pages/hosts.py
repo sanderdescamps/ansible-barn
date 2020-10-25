2584653a0ac4ab0863613078431f4d038e7de205
Sander Descamps <sander_descamps@hotmail.com>
2020-10-25 11:14:35 +0100

    fix + extend groups functionality

--- barn-server/barn/src/app/pages/hosts.py
+++ barn-server/barn/src/app/pages/hosts.py
@@ -39,17 +39,13 @@ def put_hosts(current_user=None):
     o_host = Host.objects(name=name).first()
     if o_host is None:
         try:
-            query_args = dict(name=name)
-            if "groups" in args:
-                groups = args.get("groups").split(',')
-                o_groups = Group.objects(name__in=groups)
-                query_args["groups"] = o_groups
-            o_host = Host(**query_args)
+            o_host = Host(name=name)
             resp.succeed(changed=True, status=HTTPStatus.CREATED)
         except NotUniqueError:
             resp.failed(msg='Duplicate Node: %s already exist' % (args.get("name")))
             return resp.get_response()
 
+    
     # Set variables
     barn_vars = args.get("vars", {})
     for k, v in barn_vars.items():
@@ -64,8 +60,59 @@ def put_hosts(current_user=None):
             del o_host.vars[var_to_remove]
             resp.changed()
 
+    # Save Host
     if resp.get_changed():
         o_host.save()
+
+    # Add host to group
+    o_groups_remove_list = []
+    o_groups_add_list = []
+    if args.get("groups", False):
+        groups = list_parser(args.get("groups"))
+        for group in groups:
+            o_group = Group.objects(name=group).first()
+            if not o_group and args.get('create_groups', True):
+                o_group = Group(name=group)
+                print("create %s"%(group))
+            if o_group:
+                o_groups_add_list.append(o_group)
+
+    if args.get("groups_present", False):
+        groups = list_parser(args.get("groups_present"))
+        for group in groups:
+            o_group = Group.objects(name=group).first()
+            if not o_group and args.get('create_groups', True):
+                o_group = Group(name=group)
+            if o_group:
+                o_groups_add_list.append(o_group)
+    if args.get("groups_set", False) and not any(k in args for k in ("groups", "groups_present", "groups_absent")):
+        groups = list_parser(args.get("groups_set"))
+
+        o_groups_remove = Group.objects(name__not__in=groups, hosts__in=[o_host])
+        for g in o_groups_remove:
+            o_groups_remove_list.append(g)
+        
+        for group in groups:
+            o_group = Group.objects(name=group).first()
+            if not o_group and args.get('create_groups', True):
+                o_group = Group(name=group)
+            if o_group:
+                o_groups_add_list.append(o_group)
+    if args.get("groups_absent", False):
+        groups = list_parser(args.get("groups_absent"))
+        o_groups_remove = Group.objects(name__in=groups)
+        for g in o_groups_remove:
+            o_groups_remove_list.append(g)
+    for g in list(set(o_groups_remove_list)):
+        if o_host in g.hosts:
+            g.hosts.remove(o_host)
+            g.save()
+            resp.changed()
+    for g in list(set(o_groups_add_list)):
+        if o_host not in g.hosts:
+            g.hosts.append(o_host)
+            g.save()
+            resp.changed()
 
     return resp.get_response()
 
