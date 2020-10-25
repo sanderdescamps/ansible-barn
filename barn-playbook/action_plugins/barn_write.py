cef488f96c067044204940e43a094e75037e2521
Sander Descamps <sander_descamps@hotmail.com>
2020-10-25 12:00:29 +0100

    add groups to barn_write plugin

--- barn-playbook/action_plugins/barn_write.py
+++ barn-playbook/action_plugins/barn_write.py
@@ -4,6 +4,7 @@ from ansible.plugins.action import ActionBase
 from ansible.module_utils.urls import Request
 from ansible.errors import AnsibleActionFail
 from ansible.module_utils.urls import urllib_error
+from ansible.config.manager import ensure_type
 
 
 class ActionModule(ActionBase):
@@ -24,8 +25,7 @@ class ActionModule(ActionBase):
         barn_password = module_args.get("password", None)
         token = module_args.get("token", None)
         state = module_args.get("state", None)
-        barn_vars = module_args.get("vars", {})
-        vars_to_remove = module_args.get("remove_vars",[])
+
 
         if barn_host is None:
             result['changed'] = False
@@ -36,11 +36,23 @@ class ActionModule(ActionBase):
         if state == 'present':
             try:
                 data = {
-                    "name": task_vars.get("inventory_hostname"),
-                    'type': "host",
-                    "vars": barn_vars,
-                    "vars_absent": vars_to_remove
+                    "name": task_vars.get("inventory_hostname")
                 }
+                if module_args.get("vars", None):
+                    data["vars"] = module_args.get("vars")
+                if module_args.get("remove_vars", None):
+                    data["vars_absent"] = module_args.get("remove_vars")
+                if module_args.get("groups", None):
+                    data["groups"] = ensure_type(module_args.get("groups"), 'list')
+                if module_args.get("groups_present", None):
+                    data["groups_present"] = ensure_type(module_args.get("groups_present"), 'list')
+                if module_args.get("groups_absent", None):
+                    data["groups_absent"] = ensure_type(module_args.get("groups_absent"), 'list')
+                if module_args.get("groups_set", None):
+                    if not any(k in module_args for k in ("groups", "groups_present", "groups_absent")):
+                        data["groups_set"] = ensure_type(module_args.get("groups_set"), 'list')
+                    else:
+                        self._display.warning("groups_set can't be used in combination with groups, groups_present and groups_absent. 'groups_set' will be ignored.")
 
                 query_args = dict()
                 query_args["data"] = json.dumps(data).encode('utf-8')
@@ -56,10 +68,7 @@ class ActionModule(ActionBase):
 
                 resp = Request().open("PUT", "http://%s:%s/hosts" %
                                       (barn_host, barn_port), **query_args)
-                resp_json = json.loads(resp.read())
-
-                result["changed"] = resp_json.get('changed')
-                result["vars"] = resp_json.get("host", {}).get("vars", {})
+                result = json.loads(resp.read())
 
             except urllib_error.HTTPError as e:
                 result["status"] = int(getattr(e, 'code', -1))
