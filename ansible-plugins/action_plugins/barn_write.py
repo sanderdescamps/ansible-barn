71d5e90f8b987b950a6aede8942c8922851094e1
Sander Descamps <sander_descamps@hotmail.com>
2020-11-11 11:40:37 +0100

    add install procedure + move Ansible plugins

---
+++ ansible-plugins/action_plugins/barn_write.py
@@ -0,0 +1,90 @@
+import json
+from socket import timeout
+from ansible.plugins.action import ActionBase
+from ansible.module_utils.urls import Request
+from ansible.errors import AnsibleActionFail
+from ansible.module_utils.urls import urllib_error
+from ansible.config.manager import ensure_type
+
+
+class ActionModule(ActionBase):
+
+    def run(self, tmp=None, task_vars=None):
+
+        if task_vars is None:
+            task_vars = dict()
+
+        result = super(ActionModule, self).run(tmp, task_vars)
+        del tmp  # tmp no longer has any effect
+
+        module_args = self._task.args.copy()
+
+        barn_host = module_args.get("barn_host", None)
+        barn_port = module_args.get("barn_port", 443)
+        barn_user = module_args.get("barn_user", None)
+        barn_password = module_args.get("barn_password", None)
+        token = module_args.get("barn_token", None)
+        state = module_args.get("state", None)
+
+
+        if barn_host is None:
+            result['changed'] = False
+            result['failed'] = True
+            result['msg'] = "barn_host is required"
+            return result
+
+        if state == 'present':
+            try:
+                data = {
+                    "name": task_vars.get("inventory_hostname")
+                }
+                if module_args.get("vars", None):
+                    data["vars"] = module_args.get("vars")
+                if module_args.get("remove_vars", None):
+                    data["vars_absent"] = ensure_type(module_args.get("remove_vars"), 'list')
+                if module_args.get("groups", None):
+                    data["groups"] = ensure_type(module_args.get("groups"), 'list')
+                if module_args.get("groups_present", None):
+                    data["groups_present"] = ensure_type(module_args.get("groups_present"), 'list')
+                if module_args.get("groups_absent", None):
+                    data["groups_absent"] = ensure_type(module_args.get("groups_absent"), 'list')
+                if module_args.get("groups_set", None):
+                    if not any(k in module_args for k in ("groups", "groups_present", "groups_absent")):
+                        data["groups_set"] = ensure_type(module_args.get("groups_set"), 'list')
+                    else:
+                        self._display.warning("groups_set can't be used in combination with groups, groups_present and groups_absent. 'groups_set' will be ignored.")
+
+                query_args = dict()
+                query_args["data"] = json.dumps(data).encode('utf-8')
+                query_args["headers"] = {'Content-type': 'application/json'}
+                if token:
+                    query_args["headers"]["x-access-tokens"] = token
+                if barn_user:
+                    query_args["url_username"] = barn_user
+                    query_args["force_basic_auth"] = True
+                if barn_password:
+                    query_args["url_password"] = barn_password
+                    query_args["force_basic_auth"] = True
+
+                resp = Request().open("PUT", "http://%s:%s/hosts" %
+                                      (barn_host, barn_port), **query_args)
+                result = json.loads(resp.read())
+
+            except urllib_error.HTTPError as e:
+                result["status"] = int(getattr(e, 'code', -1))
+                try:
+                    result = json.loads(e.read())
+                except AttributeError:
+                    result["status"] = 500
+                    result["error"] = "Can't parse API response to json response"
+            except timeout:
+                result["status"] = 500
+                result["error"] = "Connection timeout"
+            except Exception as e:
+                raise AnsibleActionFail(e)
+
+        elif state == "absent":
+            result["msg"] = "remove %s from barn (under construction)" % (
+                task_vars.get("inventory_hostname"))
+
+        return result
---
+++ ansible-plugins/install.sh
@@ -0,0 +1,24 @@
+mkdir -p ~/.ansible/plugins/inventory
+mkdir -p ~/.ansible/plugins/modules
+mkdir -p ~/.ansible/plugins/action_plugins
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+
+for action_plugin in "$CURRENT_DIR/action_plugins"/*.py
+do
+  echo "Create symlink: $action_plugin --> ~/.ansible/plugins/action_plugins/$(basename $action_plugin)"
+  ln -sf $action_plugin ~/.ansible/plugins/action_plugins/$(basename $action_plugin)
+done
+
+for module in "$CURRENT_DIR/modules"/*.py
+do
+  echo "Create symlink: $module --> ~/.ansible/plugins/modules/$(basename $module)"
+  ln -sf $module ~/.ansible/plugins/modules/$(basename $module)
+done
+
+for inventory in "$CURRENT_DIR/inventory_plugins"/*.py
+do
+  echo "Create symlink: $inventory --> ~/.ansible/plugins/inventory/$(basename $inventory)"
+  ln -sf $inventory ~/.ansible/plugins/inventory/$(basename $inventory)
+done
