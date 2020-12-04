67e5f920426cba149f4f6af481e2b7c765e303f2
Sander Descamps <sander_descamps@hotmail.com>
2020-12-04 09:10:44 +0100

    add logger

--- barn-server/barn/src/app/__init__.py
+++ barn-server/barn/src/app/__init__.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from flask import Flask, url_for, redirect
+from flask.logging import create_logger
 from flask_mongoengine import MongoEngine
 from flask_swagger_ui import get_swaggerui_blueprint
 from app.pages.admin.users import user_pages
@@ -14,9 +15,27 @@ from app.pages.upload import upload_pages
 from app.pages.login import login_pages
 from app.models import User
 from app.utils.config import ConfigLoader
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s:  %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 
 app = Flask(__name__)
+logger = create_logger(app)
 
 
 # Swagger
@@ -42,10 +61,10 @@ elif os.path.exists(os.path.join(os.getcwd(), "config/barn-server.cfg")):
 elif os.path.exists("/etc/barn/barn-server.cfg"):
     CFG_PATH = "/etc/barn/barn-server.cfg"
 else:
-    print("Can't load config file")
-    sys.exit("Couldn't find config file")
+    logger.info("Can't find any config file")
+    sys.exit("Can't find any config file")
 
-
+logger.info("load config file: {}".format(CFG_PATH))
 config = ConfigLoader(CFG_PATH).get_config()
 app.config.from_mapping(config)
 
@@ -58,15 +77,18 @@ app.register_blueprint(inventory_pages)
 app.register_blueprint(export_pages)
 app.register_blueprint(upload_pages)
 app.register_blueprint(login_pages)
-if config.get("BARN_CONFIG",{}).get("debug_mode", False):
+if config.get("BARN_CONFIG", {}).get("debug_mode", False):
     app.register_blueprint(debug_pages)
 
 db = MongoEngine(app)
 
-if not User.objects(name=config.get("BARN_CONFIG",{}).get("barn_init_admin_user", "admin")).first():
+if not User.objects(name=config.get("BARN_CONFIG", {}).get("barn_init_admin_user", "admin")).first():
     User(
-        name=config.get("BARN_CONFIG",{}).get("barn_init_admin_user", "admin"),
-        username=config.get("BARN_CONFIG",{}).get("barn_init_admin_user", "admin"),
-        password=config.get("BARN_CONFIG",{}).get("barn_init_admin_password", "admin"),
+        name=config.get("BARN_CONFIG", {}).get(
+            "barn_init_admin_user", "admin"),
+        username=config.get("BARN_CONFIG", {}).get(
+            "barn_init_admin_user", "admin"),
+        password=config.get("BARN_CONFIG", {}).get(
+            "barn_init_admin_password", "admin"),
         admin=True
     ).save()
