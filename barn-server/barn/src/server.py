9fddcb7fe647e4c97e63e15943759a846e1d6889
Sander Descamps <sander_descamps@hotmail.com>
2020-12-04 09:10:13 +0100

    run flask in waitress server

--- barn-server/barn/src/server.py
+++ barn-server/barn/src/server.py
@@ -1,4 +1,6 @@
 from app import app
+from waitress import serve
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    serve(app, host='0.0.0.0', port=5000,threads=4)
+    # app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
