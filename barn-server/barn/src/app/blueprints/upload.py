8c27b038508125133309f358c182e8db71016ed9
Sander Descamps <sander_descamps@hotmail.com>
2020-12-06 22:35:44 +0100

    move to login_required

--- barn-server/barn/src/app/blueprints/upload.py
+++ barn-server/barn/src/app/blueprints/upload.py
@@ -2,21 +2,22 @@ from http import HTTPStatus
 import json
 import yaml
 from flask import request, Blueprint, render_template, jsonify, redirect
+from flask_login import login_required
 from app.models import Host, Group
-from app.auth import authenticate
 
 
 upload_pages = Blueprint('upload', __name__)
 
 
 @upload_pages.route('/upload', methods=['GET'])
+@login_required
 def get_upload_file():
     return render_template('upload.html')
 
 
 @upload_pages.route('/upload', methods=['POST'])
-@authenticate("guest")
-def post_upload_file(current_user=None):
+@login_required
+def post_upload_file():
     if "file" in request.files:
         file = request.files['file']
         fileextention = file.filename.split(".")[-1]
