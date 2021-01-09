57e39ca2e09667ba57fdecaf05f96de53149ff12
Sander Descamps <sander_descamps@hotmail.com>
2021-01-09 13:21:02 +0100

    add boolean parser

--- barn-server/barn/src/app/utils/__init__.py
+++ barn-server/barn/src/app/utils/__init__.py
@@ -1,3 +1,6 @@
+import string
+import random
+
 def list_parser(to_parse):
     """
         Split a string or list of strings into seperate strings. Seperated by comma or spaces.
@@ -14,6 +17,25 @@ def list_parser(to_parse):
     else:
         output = list_parser(str(to_parse))
     return list(set(output))
+
+BOOLEANS_TRUE = frozenset(('y', 'yes', 'on', '1', 'true', 't', 1, 1.0, True))
+BOOLEANS_FALSE = frozenset(('n', 'no', 'off', '0', 'false', 'f', 0, 0.0, False))
+BOOLEANS = BOOLEANS_TRUE.union(BOOLEANS_FALSE)
+
+def boolean_parser(value, default_to_false=True):
+    if isinstance(value, bool):
+        return value
+
+    normalized_value = value
+    if isinstance(value, str):
+        normalized_value = value.lower().strip()
+
+    if normalized_value in BOOLEANS_TRUE:
+        return True
+    elif normalized_value in BOOLEANS_FALSE or default_to_false:
+        return False
+
+    raise TypeError("The value '%s' is not a valid boolean.  Valid booleans include: %s" % (value, ', '.join(repr(i) for i in BOOLEANS)))
 
 def remove_empty_fields(data):
     output = None
@@ -62,3 +84,7 @@ def remove_underscore_keys(d):
         res = list(map(lambda x: remove_underscore_keys(x)
                        if type(x) == dict else x, d))
     return res
+
+def generate_password(length=32):
+    char_set = f'{string.ascii_letters}{string.digits}/*-+&!$<>@=?%'
+    return ''.join(random.choice(char_set) for i in range(length))
