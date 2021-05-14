c1715cd3030186131a4f54acd017d6fb3f1749ad
Sander Descamps <sander_descamps@hotmail.com>
2021-05-14 16:54:28 +0200

    support bearer jwt authentication

--- barn-server/barn/src/app/auth.py
+++ barn-server/barn/src/app/auth.py
@@ -64,6 +64,8 @@ def load_user(user_id):
 def load_user_from_request(l_request):
     auth = l_request.authorization
     token = l_request.headers.get('x-access-tokens', None)
+    if not token and request.headers.get("Authorization","").startswith("Bearer"):
+        token = request.headers.get("Authorization","")[6:].strip()
     if token is not None and token != "":
         try:
             data = dict(jwt.decode(token, current_app.config["TOKEN_ENCRYPTION_KEY"], algorithms=["HS256"]))
@@ -95,6 +97,9 @@ def load_user_from_request(l_request):
 def unauthorized():
     auth = request.authorization
     token = request.headers.get('x-access-tokens', None)
+    
+    if not token and request.headers.get("Authorization","").startswith("Bearer"):
+        token = request.headers.get("Authorization","")[6:].strip()
     if not ((auth and auth.username and auth.password) or token):
         if request.user_agent.browser:
             schema = request.headers.get("X-Forwarded-Proto")
